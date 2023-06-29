64b2b2b57f11787bc5186da012fe4f231d7ae61e
BharathPeruka <bharathbharath1552005@gmail.com>
2023-06-29 10:54:55 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,20 @@
+/**
+ * test
+ */
+public class test {
+
+    int a = 2;
+    String name = "john";
+    public static void main(String[] args) {
+        int num=9;
+        test obj =new test();
+        test obj1= new test();
+
+        obj.name = "Bharath";
+
+        System.out.println(obj.a);
+        System.out.println(obj.name);
+        System.out.println(obj1.a);
+        System.out.println(obj1.name);
+    }
+}
